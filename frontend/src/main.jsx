cc53ef8642c44125834658e203ecccf69b388434
6Ssmallcoat26 <xwt13979152057@163.com>
2026-09-25 12:19:16 +0800

    Register service worker and add PWA meta tags

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
 import './index.css'
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
